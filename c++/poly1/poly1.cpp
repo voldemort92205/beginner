fc81ce5847e989f035ad37176bc6a003cc90a93d
voldemort92205 <voldemort92205@gmail.com>
2015-04-20 10:29:30 +0800

    set corre of poly

--- c++/poly1/poly1.cpp
+++ c++/poly1/poly1.cpp
@@ -12,11 +12,19 @@ class Polynomial {
 		void set_degree();
 		void set_degree(int n);
 		void set_corre();
-
+		void reset();
 		//optional
 		int get_degree ();
+		void get_corre();
 };
-
+void Polynomial :: reset (){
+	if (degree != -1){
+		delete [] corre;
+		corre = NULL;
+		degree = -1;
+		value = 0;
+	}
+}
 Polynomial :: Polynomial (){
 	corre = NULL;
 	degree = -1;
@@ -40,17 +48,32 @@ void Polynomial :: set_degree (int n){
 	}
 }
 void Polynomial :: set_corre(){
-	corre = NULL;
+	//corre = NULL;
+	if (degree != -1){
+		corre = new double [degree+1];
+		for (int i = degree; i >= 0; i --){
+			cin >> *(corre + i);
+		}
+//			cin >> degree
+	}
 }
 
 int Polynomial :: get_degree(){
 
 	return degree;
 }
+void Polynomial :: get_corre() {
+	for (int i = degree; i >= 0; i --){
+		cout << *(corre+i) << " ";
+	}
+	cout << endl;
+}
 
-int main (){
+int main (int argc, char **argv){
 	Polynomial poly1;
 	poly1.set_degree(3);
-	cout << poly1.get_degree() << endl;
+	//cout << poly1.get_degree() << endl;
+	poly1.set_corre();
+
 	return 0;
 }
